789d12a8a2c298e1b407c66ce9a3d4e993b362d9
Obi Fortune <gabrielobi.of@gmail.com>
2023-09-02 11:57:32 +0100

    added github book logic

--- src/server/api/routers/repoTools.ts
+++ src/server/api/routers/repoTools.ts
@@ -89,7 +89,76 @@ export const repoToolsRouter = createTRPCRouter({
         branch: z.string(),
       }),
     )
-    .mutation(() => {
-      return {};
+    .mutation(async ({ input }) => {
+      const { token, branch, selectedRepo, hideDirs, acceptableFileTypes } =
+        input;
+      const [owner, repo] = selectedRepo.split('/');
+
+      if (owner == undefined || repo == undefined) {
+        throw new Error('Invalid repository name');
+      }
+
+      const octokit = new Octokit({ auth: token });
+
+      const {
+        data: {
+          commit: { sha },
+        },
+      } = await octokit.rest.repos.getBranch({
+        owner,
+        repo,
+        branch,
+      });
+
+      const res = await octokit.rest.git.getTree({
+        owner,
+        repo,
+        tree_sha: sha,
+        recursive: 'true',
+      });
+
+      type FileStruct = Record<string, { files: string[]; dirs?: string[] }>;
+
+      const tree = res.data.tree.reduce<FileStruct>((acc, item) => {
+        const { path, type } = item;
+        if (path == undefined || type == undefined) return acc;
+
+        const pathParts = path.split('/');
+        if (pathParts == undefined) return acc;
+
+        const fileName = pathParts.pop();
+        if (fileName == undefined) return acc;
+
+        const dirPath = pathParts.join('/');
+        const dir = acc[dirPath] ?? (acc[dirPath] = { files: [], dirs: [] });
+
+        if (type === 'blob') {
+          if (acceptableFileTypes) {
+            const fileTypes = acceptableFileTypes
+              .split(',')
+              .map(type => type.trim())
+              .map(type => type.replace(/^\./, ''));
+            const fileType = fileName.split('.').pop();
+
+            if (!fileType || !fileTypes.includes(fileType)) {
+              return acc;
+            }
+          }
+
+          dir.files.push(fileName);
+        }
+
+        if (type === 'tree') dir.dirs?.push(fileName);
+
+        return acc;
+      }, {});
+
+      Object.keys(tree).forEach(key => {
+        if (tree[key]?.files.length === 0) delete tree[key];
+      });
+
+      if (hideDirs) Object.keys(tree).forEach(key => delete tree[key]?.dirs);
+
+      return tree;
     }),
 });
